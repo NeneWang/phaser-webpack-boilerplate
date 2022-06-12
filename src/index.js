cc07c181ad6ba1d70581867d795001bdbb666ffc
NelsonWang <wangnelson2@gmail.com>
2022-06-12 18:21:03 -0400

    index | restart | more pipes

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const config = {
 };
 
 
-function preload () {
+function preload() {
   // This is the scene - and contains the properties whens starting.
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
@@ -31,51 +31,60 @@ function preload () {
 
 
 const VELOCITY = 200;
+const PIPES_TO_RENDER = 4;
 
 let bird = null;
 
 
 let upperPipe = null;
 let lowerPipe = null;
+let pipeHorizontalDistance = 0;
 
 let pipeVerticalDistanceRange = [150, 250];
-let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange)
-let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 -pipeVerticalDistance)
 
 const FLAP_VELOCITY = 300;
-const initalBirdPosition = {x: config.width * 0.1, y: config.height / 2}
+const initalBirdPosition = { x: config.width * 0.1, y: config.height / 2 }
 
 
 
-function create () {
+function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
 
   bird = this.physics.add.sprite(initalBirdPosition.x, initalBirdPosition.y, 'bird').setOrigin(0);
   bird.body.gravity.y = 400;
-  upperPipe = this.add.sprite(400, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
-  lowerPipe = this.add.sprite(400, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    pipeHorizontalDistance += 400;
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange)
+    let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance)
+
+
+    upperPipe = this.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
+    lowerPipe = this.add.sprite(pipeHorizontalDistance, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+  }
+
 
   // bird.body.velocity.x = VELOCITY;
-  
+
   this.input.on("pointerdown", flap)
 
   this.input.keyboard.on("keydown_SPACE", flap)
 }
 
-function flap(){
-  
+function flap() {
+
   bird.body.velocity.y = -FLAP_VELOCITY;
 }
 
 
-function update(time, delta){
+function update(time, delta) {
 
-  if(bird.y > config.height || bird.y < - bird.height){
+  if (bird.y > config.height || bird.y < - bird.height) {
     restartPlayerPosition();
   }
 }
 
-function restartPlayerPosition(){
+function restartPlayerPosition() {
 
   bird.x = initalBirdPosition.x;
   bird.y = initalBirdPosition.y;
