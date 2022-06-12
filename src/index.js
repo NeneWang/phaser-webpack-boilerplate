b121eea95c7d89b1ecbff943fa125de90341a428
NelsonWang <wangnelson2@gmail.com>
2022-06-12 19:03:13 -0400

    index | restart | more pipes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 const config = {
+  // WebGL (Web graphics library) JS Api for rendering 2D and 3D graphics
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
+    // Arcade physics plugin, manages physics simulation
     default: 'arcade',
     arcade: {
       debug: true,
@@ -14,98 +15,83 @@ const config = {
   scene: {
     preload,
     create,
-    update: update
-
+    update
   }
-};
-
-
-function preload() {
-  // This is the scene - and contains the properties whens starting.
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('bird', 'assets/bird.png');
-  this.load.image('pipe', 'assets/pipe.png');
-
 }
-
 
 const VELOCITY = 200;
 const PIPES_TO_RENDER = 4;
 
 let bird = null;
+let pipes = null;
 
-
-let upperPipe = null;
-let lowerPipe = null;
 let pipeHorizontalDistance = 0;
 
-let pipeVerticalDistanceRange = [150, 250];
+const pipeVerticalDistanceRange = [150, 250];
 
-const FLAP_VELOCITY = 300;
-const initalBirdPosition = { x: config.width * 0.1, y: config.height / 2 }
+const flapVelocity = 250;
+const initalBirdPosition = {x: config.width * 0.1, y: config.height / 2}
 
-
+function preload() {
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
+}
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
-
   bird = this.physics.add.sprite(initalBirdPosition.x, initalBirdPosition.y, 'bird').setOrigin(0);
   bird.body.gravity.y = 400;
 
+  pipes = this.physics.add.group();
+
   for (let i = 0; i < PIPES_TO_RENDER; i++) {
-  
-  
-    const upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1);
-    const lowerPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 0);
-  
+    const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
+    const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0);
+
     placePipe(upperPipe, lowerPipe)
   }
 
+  pipes.setVelocityX(-200);
 
-  // bird.body.velocity.x = VELOCITY;
-
-  this.input.on("pointerdown", flap)
-
-  this.input.keyboard.on("keydown_SPACE", flap)
+  this.input.on('pointerdown', flap);
+  this.input.keyboard.on('keydown_SPACE', flap);
 }
 
-function flap() {
-
-  bird.body.velocity.y = -FLAP_VELOCITY;
-}
-
-
+// if bird y position is small than 0 or greater than height of the canvas
+// then alert "you have lost"
 function update(time, delta) {
-
-  if (bird.y > config.height || bird.y < - bird.height) {
-    restartPlayerPosition();
-  }
+ if (bird.y > config.height || bird.y < -bird.height) {
+  restartBirdPosition();
+ }
 }
 
-function placePipe(uPipe, lPipe){
+function placePipe(uPipe, lPipe) {
   pipeHorizontalDistance += 400;
-  let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange)
-  let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance)
-
-
-  // const upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
-  // const lowerPipe = this.physics.add.sprite(pipeHorizontalDistance, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+  // pipeHorizontalDistance = getRightMostPipe();
+  let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+  let pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
 
   uPipe.x = pipeHorizontalDistance;
   uPipe.y = pipeVerticalPosition;
 
-  lPipe.x = uPipe.x
+  lPipe.x = uPipe.x;
   lPipe.y = uPipe.y + pipeVerticalDistance
-
-  uPipe.body.velocity.x = -200
-  lPipe.body.velocity.x = -200
 }
 
-function restartPlayerPosition() {
+function getRightMostPipe() {
 
+
+}
+
+function restartBirdPosition() {
   bird.x = initalBirdPosition.x;
   bird.y = initalBirdPosition.y;
   bird.body.velocity.y = 0;
+}
+
+function flap() {
+  bird.body.velocity.y = -flapVelocity;
 }
 
 
