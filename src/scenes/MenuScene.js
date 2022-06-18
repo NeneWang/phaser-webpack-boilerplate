32f5385514309fbb1bd1d376494f77a5aeb4361f
NelsonWang <wangnelson2@gmail.com>
2022-06-18 07:04:48 -0400

    s2 | 38

---
+++ src/scenes/MenuScene.js
@@ -0,0 +1,27 @@
+
+import Phaser from 'phaser';
+
+class MenuScene extends Phaser.Scene{
+    constructor(){
+        super('MenuScene');
+    }
+
+    preload(){
+
+    }
+
+    create(){
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
