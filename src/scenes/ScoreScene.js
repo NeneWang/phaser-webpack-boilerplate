6125f663c15bbf391116adef0d84c31edd94db5f
NelsonWang <wangnelson2@gmail.com>
2022-06-21 20:01:26 -0400

    s2 | 445

---
+++ src/scenes/ScoreScene.js
@@ -0,0 +1,15 @@
+
+import Phaser from 'phaser';
+
+
+class ScoreScene extends Phaser.Scene {
+    constructor(config) {
+        super('ScoreScene', config);
+    }
+
+    create() {
+        super.create();
+    }
+}
+
+export default ScoreScene;
